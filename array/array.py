a0a2aa8b8dfcb5544809909bd37fe756cf3b89a2
hkPateL26 <haripatel267998@gmail.com>
2026-09-22 12:36:48 +0530

    implement bubble sort algorithm for array sorting

--- array/array.py
+++ array/array.py
@@ -1,4 +1,17 @@
 import numpy as nm
-a = nm.array([1,2,3,4,5])
-print(a)
+
+numbers = nm.array([5, 2, 8, 1, 3])
+
+for i in range(len(numbers)):
+	swapped = False
+
+	for j in range(len(numbers) - 1 - i):
+		if numbers[j] > numbers[j + 1]:
+			numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
+			swapped = True
+
+	if not swapped:
+		break
+
+print(numbers)
 
